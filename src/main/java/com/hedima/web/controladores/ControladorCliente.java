8736da687d6a7417cc27a53d7d650d9402fb02c4
TitaRuiz <ggp2002es@gmail.com>
2023-05-16 20:21:16 +0200

    Clase a las 20:21

--- src/main/java/com/hedima/web/controladores/ControladorCliente.java
+++ src/main/java/com/hedima/web/controladores/ControladorCliente.java
@@ -1,2 +1,33 @@
-package com.hedima.web.controladores;public class ControladorCliente {
+package com.hedima.web.controladores;
+
+import com.hedima.web.modelo.Cliente;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ControladorCliente {
+
+    private List<Cliente> clientes = new ArrayList<>();
+    @GetMapping("/")
+    public String mostrarInicio(){
+        return "index";
+    }
+    @GetMapping("/cliente")
+    public String mostrarFormulario(Model modelo){
+        modelo.addAttribute("cliente", new Cliente());
+        return "formulario";
+    }
+    @PostMapping("/guardar")
+    public String guardarCliente(Cliente cliente){
+        clientes.add(cliente);
+        return "index";
+    }
+
+
+
 }
--- src/main/java/com/hedima/web/modelo/Cliente.java
+++ src/main/java/com/hedima/web/modelo/Cliente.java
@@ -1,2 +1,68 @@
-package com.hedima.web.modelo;public class Cliente {
+package com.hedima.web.modelo;
+
+public class Cliente {
+
+    //1. Atributos
+    private int idCliente;
+    private String nombre;
+
+    private String domicilio;
+
+    private String pais;
+    //2. Métodos
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "idCliente=" + idCliente +
+                ", nombre='" + nombre + '\'' +
+                ", domicilio='" + domicilio + '\'' +
+                ", pais='" + pais + '\'' +
+                '}';
+    }
+
+    //3. Constructores
+    public Cliente() {}
+
+    public Cliente(int idCliente, String nombre, String domicilio, String pais) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.domicilio = domicilio;
+        this.pais = pais;
+    }
+
+    //4. Setters y getters
+
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
 }
